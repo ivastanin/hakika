dec85272c32969fd8ee5e784081e4a5ed45a0c7d
Stojan Ivastanin <ivastanin@outlook.com>
2024-11-30 13:35:45 +0100

    [ADD] Initial version of module

---
+++ hk_account/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright: 2024 Hakika Strategy Into Action (Pty) Ltd
+
+##############################################################################
+#
+#   Hakika Account
+#   Module:     hk_account
+#   Author:     Newline Software d.o.o. <office@newline.software>
+#   mail:       <office@newline.software>
+#   Copyright:  2024 Hakika Strategy Into Action (Pty) Ltd
+#
+##############################################################################
+
+{
+    'name': 'Hakika Account',
+    'version': '1.0.0',
+    'category': 'Account',
+    'summary': 'Various modifications in accounting workflow',
+    'description': """
+
+Various modifications in accounting workflow
+============================================
+
+""",
+    'author': 'Newline Software d.o.o.',
+    'website': 'https://newline.software/',
+    'depends': [
+        'account',
+    ],
+    'data': [
+    ],
+    # 'qweb': [],
+    # 'demo': [],
+    # 'css': [],
+    # 'images': [],
+    'installable': True,
+    'application': True,
+}
